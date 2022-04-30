eed8da977d6fdc3b676f9866a619f8b2769f2b6d
pyxis <windsekirun@gmail.com>
2022-04-30 16:08:56 +0900

    add conf file

--- server/src/main/kotlin/com/github/windsekirun/kohakunas/dashboard/Application.kt
+++ server/src/main/kotlin/com/github/windsekirun/kohakunas/dashboard/Application.kt
@@ -4,14 +4,15 @@ import com.github.windsekirun.kohakunas.dashboard.plugins.configureHTTP
 import com.github.windsekirun.kohakunas.dashboard.plugins.configureRouting
 import com.github.windsekirun.kohakunas.dashboard.plugins.configureSecurity
 import com.github.windsekirun.kohakunas.dashboard.plugins.configureSerialization
-import io.ktor.server.engine.*
+import io.ktor.server.application.*
 import io.ktor.server.netty.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
-        configureSerialization()
-        configureHTTP()
-        configureSecurity()
-    }.start(wait = true)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+    configureHTTP()
+    configureSecurity()
 }
