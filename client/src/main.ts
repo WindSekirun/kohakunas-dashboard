959dd7439e9c63b19a1ccf861bdbd4c587abd109
pyxis <pyxis@uzuki.live>
2022-05-05 22:28:49 +0900

    initialize vue3, vite

---
+++ client/src/main.ts
@@ -0,0 +1,12 @@
+import 'vuetify/styles' // Global CSS has to be imported
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
