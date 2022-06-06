b710bccd027e568e9e3e7b27f4ffa01cc52e7b64
pyxis <pyxis@uzuki.live>
2022-06-06 22:56:05 +0900

    Only build arm64 image if driver is supported

--- deploy.py
+++ deploy.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python
+from asyncio.subprocess import PIPE
 import configparser
 import subprocess
 import os
 import wget
 from pathlib import Path
+import platform
 
 properties = configparser.RawConfigParser()
 properties.read('local.ini')
 registry = properties.get("DEFAULT", "registry")
+
+def appendSudo(command):
+    if platform.system() == "Linux":
+        command.insert(0, "sudo")
+    return command
 
 def build(type):
     if type != 'server' and type != 'client':
         raise Exception("type must be server or client")
 
     tag = f"{registry}/windsekirun/kohakunas-dashbard/{type}"
-    buildCommand = ["docker", "buildx", "build", "--platform",
-                    "linux/amd64,linux/arm64", "-t", tag, "--push", f"./{type}"]
+
+    findPlatformCommand = appendSudo(["docker", "buildx", "inspect"])
+    findPlatformResult = subprocess.run(findPlatformCommand, stdout=PIPE, stderr=PIPE, universal_newlines=True).stdout.splitlines()[-1]
+    
+    if "arm64" in findPlatformResult:
+        supportPlatform = "linux/amd64,linux/arm64"
+    else:
+        supportPlatform = "linux/amd64"
+
+    buildCommand = appendSudo(["docker", "buildx", "build", "--platform", supportPlatform, "-t", tag, "--push", f"./{type}"])
 
     print(" ".join(buildCommand))
 
